7fac3de5621ec49dc531e0beb0f7f220d50f21fd
SarangRao20 <raosarang2006@gmail.com>
2025-07-27 09:31:56 +0530

    Testing Groq api with Tesseract-OCR

---
+++ main.py
@@ -0,0 +1,46 @@
+from pytesseract import pytesseract
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+class OCR:
+    def __init__(self):
+        self.path = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    
+    def extract(self, filename): 
+        try:
+            pytesseract.tesseract_cmd = self.path
+
+            text = pytesseract.image_to_string(filename)
+
+            return text
+        except Exception as e:
+            print(e)
+            return "Error"
+    
+    def mcqs_generation(self, text):
+        try:
+            count = int(input("Enter number of mcqs you wish: "))
+            difficulty = input("Enter the difficulty level for questions: ")
+            chat_completion = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": f" {text} use this content and give me {count} mcqs of {difficulty} difficulty  in json format ",
+                    }
+                ],
+                model="llama3-8b-8192",
+            )
+
+            print(chat_completion.choices[0].message.content)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+ocr = OCR()
+text = ocr.extract("ncert-1.png")
+ocr.mcqs_generation(text)
