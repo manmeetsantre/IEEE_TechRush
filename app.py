8fec5270d2b404b7716f8baf1d3b872672c6beac
Vedant Jadhav <greedyprontocomponent@tuta.io>
2025-08-09 17:04:46 +0530

    Updated icons

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def extract_text_from_pdf(pdf_file):
     images = convert_from_bytes(pdf_bytes, dpi=75, grayscale=True)
     text = ''
     for image in images:
-        text += pytesseract.image_to_string(image, lang='eng+hin+mar', config='--psm 6')
+        text += pytesseract.image_to_string(image)
     return text, "ocr"
 
 # sends summary request to local Mistral (Ollama) API
